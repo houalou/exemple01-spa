9a7e80b6a80cc1544f2db501336657f0718a05a3
houalou <houalou@yahoo.fr>
2025-02-15 22:37:35 -0500

    Un autre commit

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,8 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
+
+
 git init
 git add -A
 
@@ -24,6 +26,7 @@ git commit -m "Initial commit"
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:houalou/exemple01-spa.git main:gh-pages
+#git push -f git@github.com:houalou/exemple01-spa.git main:gh-pages
 
-cd -
+git remote add origin https://github.com/houalou/exemple01-spa.git
+git push -u origin master
